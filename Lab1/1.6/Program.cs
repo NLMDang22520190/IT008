0a2e07811965a6294edfb1dfddcf540de29f68d8
MaiThanhBach22520090 <146149281+MaiThanhBach22520090@users.noreply.github.com>
2023-10-08 05:10:28 +0700

    Add files via upload

---
+++ Lab1/1.6/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _1._6
+{
+    interface IAbility : IIntelligent, IThingking
+    { }
+
+    interface IIntelligent
+    {
+        void intelligent_behavior();
+    }
+
+    interface IThingking
+    {
+        void intelligent_behavior();
+    }
+
+    class Mamal
+    {
+        string characteristics;
+    }
+
+    class Whale : Mamal
+    {
+        public Whale() { }
+    }
+
+    class Human : Mamal, IAbility
+    {
+        public Human() { }
+        public void intelligent_behavior()
+        {
+            Console.WriteLine("Steven has stopped thinking...");
+        }
+        public void thinking_behavior()
+        {
+            Console.WriteLine("Steven tries to use 'Failure' - It failed...");
+        }
+    }
+
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Steven = new Human();
+            Steven.thinking_behavior();
+            Steven.intelligent_behavior();
+        }
+    }
+}
