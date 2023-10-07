38011290a40c804a3736980ba7f5629d4c9d8f74
Nguyễn Lưu Minh Đăng <146148978+NLMDang22520190@users.noreply.github.com>
2023-10-07 20:38:52 +0700

    Add files via upload

---
+++ Lab1/1.5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            Console.Write("Nhap vao duong dan:");
+            path = Console.ReadLine();
+            Console.WriteLine(path);
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Thu muc ton tai: ");
+                string[] filePath = Directory.GetDirectories(path);
+                foreach (var item in filePath)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+                Console.WriteLine("Duong dan khong ton tai");
+
+
+        }
+    }
+}
